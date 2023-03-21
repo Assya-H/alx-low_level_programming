7961b86052fa7da2339ee8947bd1f5036ab27edb
Sitty <sitty@gmail.com>
2023-03-21 16:20:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print the alphabet in lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
